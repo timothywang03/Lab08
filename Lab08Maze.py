d25615c9fccd86135ec0505e8b791cceefd1e0b7
Timothy Wang <curioustimwang@gmail.com>
2021-12-14 21:28:23 -0800

    Update Lab08Maze.py

--- Lab08Maze.py
+++ Lab08Maze.py
@@ -5,37 +5,38 @@ from Maze import Maze
 from UIMaze import UI
 
 def main():
-    UI = UI()   # calls the UI class
+    ui = UI()   # calls the ui class
 
-    cur = UI.get_win().getMouse()   # gets current click
+    cur = ui.get_win().getMouse()   # gets current click
 
     while True:
 
         # quit
-        if UI.get_quit().clicked(cur):
-            UI.get_win().close()
+        if ui.get_quit().clicked(cur):
+            ui.get_win().close()
+            break
 
         # if generate is clicked
-        elif UI.get_generate().clicked(cur):
-            dimensions = UI.get_dimensions()    # grabs dimensions
+        elif ui.get_generate().clicked(cur):
+            dimensions = ui.get_dimensions()    # grabs dimensions
             maze = Maze(dimensions)
             maze.fill()                         # fills the maze in board module
             maze = maze.get_maze()
-            top_left = 350 - (dimensions[0]) * UI.get_square_size() # instantiates top left corner of maze
+            top_left = 350 - (dimensions[0]) * ui.get_square_size() # instantiates top left corner of maze
 
             # draws the squares within the maze and colors them in
             for x in range(dimensions[0] * 2 + 1):
                 for y in range(dimensions[1] * 2 + 1):
-                    rect = Rectangle(Point(top_left + x * UI.get_square_size(), top_left + y * UI.get_square_size() + 100),
-                                Point(top_left + (x + 1) * UI.get_square_size(), top_left + (y + 1) * UI.get_square_size() + 100))
+                    rect = Rectangle(Point(top_left + x * ui.get_square_size(), top_left + y * ui.get_square_size() + 100),
+                                Point(top_left + (x + 1) * ui.get_square_size(), top_left + (y + 1) * ui.get_square_size() + 100))
                     if maze[x][y] == 'W':
                         rect.setFill('brown')
                         rect.setOutline('brown')
                     else:
                         rect.setFill('green')
                         rect.setOutline('green')
-                    rect.draw(UI.get_win())
+                    rect.draw(ui.get_win())
 
-        cur = UI.get_win().getMouse()
+        cur = ui.get_win().getMouse()
 
 main()
